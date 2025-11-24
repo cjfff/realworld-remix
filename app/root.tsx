61738fb2322eb40f2d774baa137f0f6ed4981bef
“Maple” <wiley@ku.com>
2025-11-24 18:23:20 +0800

    feat: project init

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,8 @@ import {
 
 import type { Route } from "./+types/root";
 import "./app.css";
+import Nav from "~/components/Nav";
+import { Footer } from "./components/footer/index";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -27,13 +29,27 @@ export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
+        <title>Conduit</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          href="//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"
+          rel="stylesheet"
+          type="text/css"
+        />
+        <link
+          href="//fonts.googleapis.com/css?family=Titillium+Web:700|Source+Serif+Pro:400,700|Merriweather+Sans:400,700|Source+Sans+Pro:400,300,600,700,300italic,400italic,600italic,700italic"
+          rel="stylesheet"
+          type="text/css"
+        />
+        <link rel="stylesheet" href="//demo.productionready.io/main.css" />
         <Meta />
         <Links />
       </head>
       <body>
+        <Nav />
         {children}
+        <Footer />
         <ScrollRestoration />
         <Scripts />
       </body>
---
+++ app/routes/profile.$username._index.tsx
@@ -0,0 +1,10 @@
+import { Outlet } from "react-router";
+
+export default () => {
+  return (
+    <div>
+      profile.username
+
+    </div>
+  );
+};
