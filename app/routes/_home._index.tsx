677af8f1f853a0087135654004aab917aadef13d
“Maple” <wiley@ku.com>
2025-11-25 18:30:04 +0800

    feat: home page

--- app/routes/_home._index.tsx
+++ app/routes/_home._index.tsx
@@ -1,26 +1,27 @@
 import { useLoaderData } from "react-router";
-import type { Route } from "./+types/profile.$username._index";
 import fetchClient from "~/libs/api";
 import Articles from "~/components/Articles";
+import type { Route } from "./+types/_home._index";
 
-export async function loader({ params, request }: Route.LoaderArgs) {
-  const username = params.username;
+export async function loader({ request }: Route.LoaderArgs) {
   const url = new URL(request.url);
 
   const searchParams = Object.fromEntries(url.searchParams.entries()) as {
     page: string;
     size: string;
+    tag: string
   };
 
   const page = searchParams?.page || 1;
   const size = searchParams?.size || 10;
+  const tag = searchParams?.tag || "";
 
   const res = await fetchClient.GET("/articles", {
     params: {
       query: {
         limit: Number(size),
         offset: Number(size) * (Number(page) - 1),
-        author: username,
+        tag
       },
     },
   });
--- app/routes/_home.feed.tsx
+++ app/routes/_home.feed.tsx
@@ -1,29 +1,30 @@
 import { useLoaderData } from "react-router";
-import type { Route } from "./+types/profile.$username._index";
 import fetchClient from "~/libs/api";
 import Articles from "~/components/Articles";
+import type { Route } from "./+types/_home._index";
 
-export async function loader({ params, request }: Route.LoaderArgs) {
-  const username = params.username;
+export async function loader({ request }: Route.LoaderArgs) {
   const url = new URL(request.url);
 
   const searchParams = Object.fromEntries(url.searchParams.entries()) as {
     page: string;
     size: string;
+    tag: string;
   };
 
   const page = searchParams?.page || 1;
   const size = searchParams?.size || 10;
+  const tag = searchParams?.tag || "";
 
-  const res = await fetchClient.GET("/articles", {
+  const res = await fetchClient.GET("/articles/feed", {
     params: {
       query: {
         limit: Number(size),
         offset: Number(size) * (Number(page) - 1),
-        author: username,
       },
     },
   });
+
   return {
     ...res.data,
     page: Number(page),
