f8e96e662120649ce5c219ea48611ec01455e5f5
“Maple” <wiley@ku.com>
2025-11-25 16:15:41 +0800

    feat: enhance the api security

--- app/routes/api.follow.ts
+++ app/routes/api.follow.ts
@@ -2,26 +2,36 @@ import { redirect, type ActionFunctionArgs } from "react-router";
 import fetchClient from "~/libs/api";
 import { checkIsLogin } from "~/session.server";
 
+export async function loader({}: ActionFunctionArgs) {
+  throw new Response("Page not found", {
+    status: 404,
+  });
+}
+
 export async function action({ request }: ActionFunctionArgs) {
   if (await checkIsLogin(request)) {
-    return redirect('/login')
+    return redirect("/login");
   }
   let formData = await request.formData();
   const data = Object.fromEntries(formData);
   const { action, username } = data as {
-    username: string
-    action: 'follow' | 'unfollow';
+    username: string;
+    action: "follow" | "unfollow";
   };
 
   const params = {
     params: {
       path: {
-        username
-      }
-    }
-  }
+        username,
+      },
+    },
+  };
 
-  await (action === 'follow' ? fetchClient.POST('/profiles/{username}/follow', params) : fetchClient.DELETE('/profiles/{username}/follow', params))
-  
+  await (action === "follow"
+    ? fetchClient.POST("/profiles/{username}/follow", params)
+    : fetchClient.DELETE("/profiles/{username}/follow", params));
+
   return true;
 }
+
+export { ErrorBoundary } from "~/root";
